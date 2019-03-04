66a6b7b3e95b2ac76cdbdcef2d605d484abacae0
skywex <vlad_soltan@mail.ru>
2019-03-05 02:21:38 +0300

    v.0.2

--- helpers/wotb_account_info.php
+++ helpers/wotb_account_info.php
@@ -4,6 +4,12 @@ require_once("../helpers/grabber.php");
 $application_id = "85ef22379a59f6a0dba28174be9a0902";
 $url_wotb_account_list = "https://api.wotblitz.ru/wotb/account/list/";
 $url_wotb_account_info = "https://api.wotblitz.ru/wotb/account/info/";
+$url_wotb_tanks_stat = "https://api.wotblitz.ru/wotb/tanks/stats/";
+$url_wotb_encyclopedia = "https://api.wotblitz.ru/wotb/encyclopedia/vehicles/";
+$url_wotb_account_achievements = "https://api.wotblitz.ru/wotb/account/achievements/";
+$url_wotb_clans_accountinfo = "https://api.wotblitz.ru/wotb/clans/accountinfo/";
+// $url_wgn_account_list = "https://api.worldoftanks.ru/wgn/account/list/";
+// $url_wgn_servers_info = "https://api.worldoftanks.ru/wgn/servers/info/";
 
 if (isset($_POST["search"])) {
     $nickname = $_POST["search"];
@@ -13,8 +19,31 @@ if (isset($_POST["search"])) {
     
     if (isset($account_id)) {
         $account_info_response = json_decode(grab("$url_wotb_account_info?application_id=$application_id&account_id=$account_id"), true);
-        $statistics_all = $account_info_response["data"][$account_id]["statistics"]["all"];
-        
+        $tanks_stat_response = json_decode(grab("$url_wotb_tanks_stat?application_id=$application_id&account_id=$account_id"), true);
+        $account_achievements_response = json_decode(grab("$url_wotb_account_achievements?application_id=$application_id&account_id=$account_id"), true);
+
+        $slice_offset = ceil(count($tanks_stat_response["data"][$account_id]) / 100);
+        $sliced_tanks_array = [];
+        for ($i = 0; $i < $slice_offset; $i++) {
+            $sliced_tanks_array += array($i => array_slice($tanks_stat_response['data'][$account_id], $i * 100, 100));
+        }
+
+        $tanks_encyclopedia = [];
+        foreach ($sliced_tanks_array as $tanks_array) {
+            $tanks_ids = "";
+            foreach ($tanks_array as $tank) {
+                $tanks_ids .= ",".$tank["tank_id"];
+            }
+
+            $tanks_encyclopedia_response = json_decode(grab("$url_wotb_encyclopedia?application_id=$application_id&tank_id=$tanks_ids&fields=tier,name,tank_id"), true);
+            $tanks_encyclopedia += $tanks_encyclopedia_response["data"];
+        }
+
+        $statistics_all = $account_info_response["data"][$account_id]
+        + array("tanks_stat" => $tanks_stat_response["data"][$account_id])
+        + array("tanks_encyclopedia" => $tanks_encyclopedia)
+        + array("achievements" => $account_achievements_response["data"][$account_id]);
+        // clan
         echo json_encode($statistics_all);
     } else echo json_encode(array("error" => "Such user`s nickname not found."));
 }
--- pages/wotb_account_info.php
+++ pages/wotb_account_info.php
@@ -1,7 +1,21 @@
 <?php
 if (isset($_POST["statistics_all"])) {
-    $statistics_all = json_decode($_POST["statistics_all"]);
+    // statitics: info, all, tanks, achievents;
+    $account_info = json_decode($_POST["statistics_all"]);
+    $statistics_all = $account_info->statistics->all; 
+    $tanks_stat = $account_info->tanks_stat;
+    $tanks_encyclopedia = $account_info->tanks_encyclopedia;
+    $achievements = $account_info->achievements;
+    $account_achievements = $achievements->achievements;
+    $account_max_series = $achievements->max_series;
 
+    // info;
+    $nickname = $account_info->nickname;
+    $created_at = date("d.m.Y H:i:s", $account_info->created_at);
+    $updated_at = date("d.m.Y H:i:s", $account_info->updated_at);
+    $last_battle_time = date("d.m.Y H:i:s", $account_info->last_battle_time);
+
+    // all;
     $spotted = $statistics_all->spotted;
     $max_frags_tank_id = $statistics_all->max_frags_tank_id;
     $hits = $statistics_all->hits; 
@@ -21,50 +35,152 @@ if (isset($_POST["statistics_all"])) {
     $win_and_survived = $statistics_all->win_and_survived; 
     $survived_battles = $statistics_all->survived_battles; 
     $dropped_capture_points = $statistics_all->dropped_capture_points;
+    $joint_victory = $account_max_series->jointVictory;
+    $masters = $account_achievements->markOfMastery;
+
+    // calculated values from statistics->all;
+    $wins_percent = $wins / $battles * 100;
+    $wins_percent_rounded = round($wins_percent, 2);
+    $draw = $battles - $wins - $losses;
+    $avg_damage_dealt = $damage_dealt / $battles;
+    $avg_damage_dealt_rounded = round($avg_damage_dealt);
+    $damage_rate = $damage_dealt / $damage_received;
+    $damage_rate_rounded = round($damage_rate, 2);
+    $destruction_rate = $frags / $survived_battles;
+    $destruction_rate_rounded = round($destruction_rate, 2);
+    $avg_frags = $frags / $battles;
+    $avg_frags_rounded = round($avg_frags, 2);
+    $avg_spotted = $spotted / $battles;
+    $avg_spotted_rounded = round($avg_spotted, 2);    
+    $avg_capture = $capture_points / $battles;
+    $avg_capture_rounded = round($avg_capture, 2);
+    $avg_dropped_capture = $dropped_capture_points / $battles;
+    $avg_dropped_capture_rounded = round($avg_dropped_capture, 2);
+    $hits_percent = $hits / $shots * 100;
+    $hits_percent_rounded = round($hits_percent, 2);
+    $survival_percent = $survived_battles / $battles * 100;
+    $survival_percent_rounded = round($survival_percent, 2);
+    $avg_xp = $xp / $battles;
+    $avg_xp_rounded = round($avg_xp);
+    $avg_damage_received = round($damage_received / $battles);    
+    $solo_wins = $wins - $joint_victory;
+    $joint_victory_percent = round($joint_victory / $wins * 100, 2);
+    $avg_shots = round($shots / $battles, 2);
+    $avg_hits = round($hits / $battles, 2);
+
+    // tanks;
+    $tanks_names = [];
+    $tanks_tiers = [];
+    $max_tier = 0;
+    $tanks_counter = count($tanks_stat);
+    foreach ($tanks_encyclopedia as $tank) {
+        if ($tank->tier > $max_tier) $max_tier = $tank->tier;
+
+        $tanks_names[$tank->tank_id] = $tank->name;
+        $tanks_tiers[$tank->tank_id] = $tank->tier;
+    }
+
+    foreach ($tanks_stat as $tank) {
+        $tank_id = $tank->tank_id;
+        $tank_battles = $tank->all->battles;
+        $tank_tier = $tanks_tiers[$tank_id];
+
+        $total_tier_battles += $tank_tier * $tank_battles;
+    }
+
+    $avg_tier = $total_tier_battles / $battles;
+    $avg_tier_rounded = round($avg_tier, 2);
+
+    // ratings;
+    $KPD = round($avg_damage_dealt * (10 / ($avg_tier + 2)) * (0.23 + 2 * $avg_tier / 100) + $avg_frags * 250 + $avg_spotted * 150 + (log($avg_capture + 1, 1.732)) * 150 + $avg_dropped_capture * 150, 2);				
+    $WN6 = round((1240 - 1040 / pow(min($avg_tier, 6), 0.164)) * $avg_frags + $avg_damage_dealt * 530 / (184 * exp(0.24 * $avg_tier) + 130) + $avg_spotted * 125 + min($avg_dropped_capture, 2.2) * 100 + ((185 / (0.17 + exp(($wins_percent - 35) * -0.134))) - 500) * 0.45 + (6 - min($avg_tier, 6)) * -60, 2);
+    $WN7 = round((1240 - 1040 / (pow(min($avg_tier, 6), 0.164))) * $avg_frags + $avg_damage_dealt * 530 / (184 * exp(0.24 * $avg_tier) + 130) + $avg_spotted * 125 * min($avg_tier, 3) / 3 + min($avg_dropped_capture, 2.2) * 100 + ((185 / (0.17 + exp((($wins_percent) - 35) * -0.134))) - 500) * 0.45 + (-1 * (((5 - min($avg_tier, 5)) * 125) / (1 + exp(($avg_tier - ($battles / 220^(3 / $avg_tier))) * 1.5)))), 2);
+    
+
+echo<<<HERE
+<table>
+    <tr><td>Ник<td> <td>$nickname</td></tr>
+    <tr><td>Зарегистрирован<td> <td>$created_at</td></tr>
+    <tr><td>Обновлено<td> <td>$updated_at</td></tr>
+    <tr><td>Последний раз был в бою<td> <td>$last_battle_time</td></tr>
+    <tr><td>---</td></tr>
+    <tr><td>КПД</td> <td>$KPD<td></tr>
+    <tr><td>WN6</td> <td>$WN6<td></tr>
+    <tr><td>WN7</td> <td>$WN7<td></tr>
+    <tr><td>---</td></tr>
+    <tr><td>Всего боёв</td> <td>$battles<td></tr>
+    <tr><td>Процент побед</td> <td>$wins_percent_rounded %<td></tr>
+    <tr><td>Общее количество побед</td> <td>$wins<td></tr>
+    <tr><td>Поражения</td> <td>$losses<td></tr>
+    <tr><td>Ничьи</td> <td>$draw<td></tr>
+    <tr><td>---</td></tr>
+    <tr><td>Из них побед взводом (%)</td> <td>$joint_victory_percent %<td></tr>
+    <tr><td>Взводные победы</td> <td>$joint_victory<td></tr>
+    <tr><td>Соло победы</td> <td>$solo_wins<td></tr>
+    <tr><td>Средний уровень танков</td> <td>$avg_tier_rounded<td></tr>
+    <tr><td>---</td></tr>
+    <tr><td>Общий урон</td> <td>$damage_dealt<td></tr>
+    <tr><td>Уничтожено</td> <td>$frags<td></tr>
+    <tr><td>Обнаружено</td> <td>$spotted<td></tr>
+    <tr><td>Очков захвата базы</td> <td>$capture_points<td></tr>
+    <tr><td>Очков защиты базы</td> <td>$dropped_capture_points<td></tr>
+    <tr><td>Произведено выстрелов</td> <td>$shots<td></tr>
+    <tr><td>Выжил в боях</td> <td>$survived_battles<td></tr>
+    <tr><td>Получено опыта</td> <td>$xp<td></tr>
+    <tr><td>---</td></tr>
+    <tr><td>Средний урон</td> <td>$avg_damage_dealt_rounded<td></tr>
+    <tr><td>Коэф. урона</td> <td>$damage_rate_rounded<td></tr>
+    <tr><td>Коэф. уничтожения</td> <td>$destruction_rate_rounded<td></tr>
+    <tr><td>Убито за бой</td> <td>$avg_frags_rounded<td></tr>
+    <tr><td>Обнаружено за бой</td> <td>$avg_spotted_rounded<td></tr>
+    <tr><td>Захват базы за бой</td> <td>$avg_capture_rounded<td></tr>
+    <tr><td>Защита базы за бой</td> <td>$avg_dropped_capture_rounded<td></tr>
+    <tr><td>Процент попадания</td> <td>$hits_percent_rounded %<td></tr>
+    <tr><td>Процент выживания</td> <td>$survival_percent_rounded %<td></tr>
+    <tr><td>Средний опыт за бой</td> <td>$avg_xp_rounded<td></tr>
+    <tr><td>Вытанковано в среднем</td> <td>$avg_damage_received<td></tr>
+    <tr><td>Максимальный опыт за бой </td> <td>$max_xp<td></tr>
+    <tr><td>на танке</td> <td>$tanks_names[$max_xp_tank_id]<td></tr>
+    <tr><td>Максимальный уровень техники</td> <td>$max_tier</td></tr>
+    <tr><td>Использовано техники</td> <td>$tanks_counter</td></tr>
+    <tr><td>В среднем выстрелов за бой</td> <td>$avg_shots</td></tr>
+    <tr><td>В среднем попаданий за бой</td> <td>$avg_hits</td></tr>
+    <tr><td>Количество мастеров</td> <td>$masters</td></tr>
+</table>
+<hr>
+
+HERE;
+echo "<table>";
+echo "<tr><td>Уровень</td> <td>Танк</td> <td>Бои</td> <td>Победы</td> <td>Урон</td> <td>Ср.опыт</td></tr>";
+    foreach ($tanks_stat as $tank) {
+        $tank_id = $tank->tank_id;
+        $tank_tier = $tanks_tiers[$tank_id] ? $tanks_tiers[$tank_id] : "<i>hidden</i>";
+        $tank_name = $tanks_names[$tank_id] ? $tanks_names[$tank_id] : "<i>hidden</i>";
+        $tank_battles = $tank->all->battles;
+        $tank_wins = $tank->all->wins;
+        $tank_wins_percent = round($tank_wins / $tank_battles * 100, 2);
+        $tank_damage_dealt = $tank->all->damage_dealt;
+        $tank_avg_damage_dealt = round($tank_damage_dealt / $tank_battles);
+        $tank_xp = $tank->all->xp;
+        $tank_avg_xp = round($tank_xp / $tank_battles);
+
+        echo "<tr><td>$tank_tier</td> <td>$tank_name</td> <td>$tank_battles</td> <td>$tank_wins_percent %</td> <td>$tank_avg_damage_dealt</td> <td>$tank_avg_xp</td></tr>";
+    }
+echo "</table><hr>";
+
+echo "<table>";
+echo "<tr><td>Медаль</td> <td>Количество</td></tr>";
+foreach ($account_achievements as $medal => $count) {
+    echo "<tr><td>$medal</td> <td>$count</td></tr>";
+}
+echo "</table><hr>";
+
+echo "<table>";
+echo "<tr><td>Серия</td> <td>Значение</td></tr>";
+foreach ($account_max_series as $series => $value) {
+    echo "<tr><td>$series</td> <td>$value</td></tr>";
+}
+echo "</table>";
+
 }
 ?>
-
-<table>
-    <tr><td>КПД</td> <td>0<td></tr>
-    <tr><td>WN6</td> <td>0<td></tr>
-    <tr><td>WN7</td> <td>0<td></tr>
-    <tr><td>Бронесайт</td> <td>0<td></tr>
-    <tr><td>---</td></tr>
-    <tr><td>Всего боёв</td> <td>0<td></tr>
-    <tr><td>Процент побед</td> <td>0<td></tr>
-    <tr><td>Общее количество побед</td> <td>0<td></tr>
-    <tr><td>Поражения</td> <td>0<td></tr>
-    <tr><td>---</td></tr>
-    <tr><td>Из них побед взводом (%)</td> <td>0<td></tr>
-    <tr><td>Из них побед соло (%)</td> <td>0<td></tr>
-    <tr><td>Взводные победы</td> <td>0<td></tr>
-    <tr><td>Соло победы</td> <td>0<td></tr>
-    <tr><td>Средний уровень танков</td> <td>0<td></tr>
-    <tr><td>---</td></tr>
-    <tr><td>Общий урон</td> <td>0<td></tr>
-    <tr><td>Уничтожено</td> <td>0<td></tr>
-    <tr><td>Обнаружено</td> <td>0<td></tr>
-    <tr><td>Очков захвата базы</td> <td>0<td></tr>
-    <tr><td>Очков защиты базы</td> <td>0<td></tr>
-    <tr><td>Произведено выстрелов</td> <td>0<td></tr>
-    <tr><td>Выжил в боях</td> <td>0<td></tr>
-    <tr><td>Получено опыта</td> <td>0<td></tr>
-    <tr><td>---</td></tr>
-    <tr><td>Средний урон</td> <td>0<td></tr>
-    <tr><td>Коэф. урона</td> <td>0<td></tr>
-    <tr><td>Коэф. уничтожения</td> <td>0<td></tr>
-    <tr><td>Убито за бой</td> <td>0<td></tr>
-    <tr><td>Обнаружено за бой</td> <td>0<td></tr>
-    <tr><td>Захват базы за бой</td> <td>0<td></tr>
-    <tr><td>Защита базы за бой</td> <td>0<td></tr>
-    <tr><td>Процент попадания</td> <td>0<td></tr>
-    <tr><td>Процент выживания</td> <td>0<td></tr>
-    <tr><td>Средний опыт за бой</td> <td>0<td></tr>
-    <tr><td>Вытанковано в среднем</td> <td>0<td></tr>
-    <tr><td>Максимальный опыт за бой </td> <td>0<td></tr>
-    <tr><td>на танке</td> <td>0<td></tr>
-    <tr><td>---</td></tr>
-    <tr><td>Медали</td></tr>
-    <tr><td>---</td></tr>
-    <tr><td>Танки</td></tr>
-</table>
