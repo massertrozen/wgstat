6559eedda4f6bdabf4cb7f69f2fe5f288e1209aa
skywex <vlad_soltan@mail.ru>
2019-04-17 22:01:49 +0300

    main page

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <html>
 <head>
     <title>WGstat</title>
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/jquery.cookie.js"></script>
-    <script type="text/javascript" src="js/main.js"></script>
-
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.cookie.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon" />
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/material-icons.css" rel="stylesheet">
 </head>
-<body>
+<body class="main">
+    <noscript>To work with this service you need to swith ON your browser JavaScript.</noscript>
     <div class="main-container">
-        <form class="find-nickname">
-            <input type="text" name="nickname" placeholder="Кого найти?" autofocus />
-            <button type="submit">Вперёд</button>
-            <div class="find-nickname-error"></div>
-        </form>    
-    </div>
+        <div class="main-content">
+            <form class="find-nickname">
+                <input type="text" class="main-input search" name="nickname" placeholder="Кого найти?" autofocus required />
+                <button type="submit" class="main-button">Вперёд</button>
+                <div class="find-nickname-error"></div>
+            </form>
+        </div>
+
+        <div class="main-footer">
+            <div class="footer-author">
+                <a href="https://vk.com/massertrozen"><i class="material-icons">copyright</i> <span>SkyWex</span> dev.</a>
+            </div>
+            <div class="footer-about">
+                <span>WGstat</span> — сервис просмотра статистики по играм Wargaming.net
+            </div>
+        </div>   
+    </div>   
 </body>
 </html>
