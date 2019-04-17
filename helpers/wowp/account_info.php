478bbec6ed5e4a580aaedffeee8c8adbf1fe55ce
skywex <vlad_soltan@mail.ru>
2019-04-17 22:05:27 +0300

    error message translation to main lang

--- helpers/wowp/account_info.php
+++ helpers/wowp/account_info.php
@@ -6,7 +6,7 @@ require_once("../urls.php");
 if (isset($_POST["search"])) {
     $nickname = $_POST["search"];
     $output = array(
-        "error" => "This page in development. See you soon...",
+        "error" => "Страница в разработке. Скоро...",
         "game" => "wowp", 
         "nickname" => $nickname
     );
